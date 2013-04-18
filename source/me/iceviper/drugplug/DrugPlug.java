a44e879da587bf53bb1133a052a58e0c837f3a48
1ceviper <jeroenvanwier@gmail.com>
2013-04-18 22:30:43 +0200

    Main Plugin File

--- source/me/iceviper/drugplug/DrugPlug.java
+++ source/me/iceviper/drugplug/DrugPlug.java
@@ -1,15 +1,19 @@
 package me.iceviper.drugplug;
 
+import java.util.ArrayList;
+
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DrugPlug extends JavaPlugin{
 	
-	
+	ArrayList<Material> drugs = new ArrayList<Material>();
 	
 	
 	@Override
 	public void onEnable() {
 		getLogger().info("[DrugPlug] Has been enabled, have fun drugging!");
+		
 	}
 	
 	@Override
