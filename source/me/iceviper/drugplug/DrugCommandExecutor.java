ae46efb57064dd02c27068b35a13f1e4414a92b8
1ceviper <jeroenvanwier@gmail.com>
2013-04-19 18:03:52 +0200

    First Version Command Executor

--- source/me/iceviper/drugplug/DrugCommandExecutor.java
+++ source/me/iceviper/drugplug/DrugCommandExecutor.java
@@ -21,28 +21,47 @@ public class DrugCommandExecutor implements CommandExecutor{
 			return true;
 		} else {
 			if (args[0] == "add") {
-				if (!cmdSender.hasPermission("drug.add") || !(cmdSender instanceof Player)) {
+				if (!cmdSender.hasPermission("drug.add")) {
 					cmdSender.sendMessage(ChatColor.RED + "You do not have permission to add drugs to the config, this is Admin Only!");
 					return true;
 				}
 				if (args.length != 2) {
-					cmdSender.sendMessage("'/drug add <id[:data]>' only accepts 2 arguments! You put in " + args.length);
+					cmdSender.sendMessage(ChatColor.RED + "'/drug add <id[:data]>' only accepts 2 arguments! You put in " + args.length);
 				} else {
 					plugin.getConfig().set("drugs", plugin.getConfig().getStringList("drugs").add(args[1]));
+					plugin.saveConfig();
 					plugin.reloadDrugConfig();
 				}
 				return true;
 			} else if (args[0] == "reload") {
-				if (!cmdSender.hasPermission("drug.reload") || !(cmdSender instanceof Player)) {
+				if (!cmdSender.hasPermission("drug.reload")) {
 					cmdSender.sendMessage(ChatColor.RED + "You do not have permission to reload the config, this is Admin Only!");
 					return true;
 				}
 				plugin.reloadDrugConfig();
 				return true;
 			} else if (args[0] == "newjail") {
+				if (!cmdSender.hasPermission("drug.newjail") || !(cmdSender instanceof Player)) {
+					cmdSender.sendMessage(ChatColor.RED + "You do not have permission to create new jails, this is Admin Only!");
+					return true;
+				}
 				int jailnum = plugin.getConfig().getInt("jails.amount") + 1;
 				plugin.getConfig().set("jails.amount", jailnum);
-				plugin.getConfig().set("jails." + jailnum + "", ((Player) cmdSender).getLocation());
+				plugin.getConfig().set("jails." + jailnum + ".x", ((Player) cmdSender).getLocation().getBlockX());
+				plugin.getConfig().set("jails." + jailnum + ".y", ((Player) cmdSender).getLocation().getBlockY());
+				plugin.getConfig().set("jails." + jailnum + ".z", ((Player) cmdSender).getLocation().getBlockZ());
+				plugin.saveConfig();
+				return true;
+			} else if (args[0] == "jail") {
+				if (!cmdSender.hasPermission("drug.jail")) {
+					cmdSender.sendMessage(ChatColor.RED + "You do not have permission to jail people, this is Admin Only!");
+					return true;
+				}
+				if (args.length != 3) {
+					cmdSender.sendMessage(ChatColor.RED + "'/drug jail <player> <time in seconds>' only accepts 3 arguments! You put in " + args.length);
+					return true;
+				}
+				plugin.jail(plugin.getServer().getPlayer(args[1]), Integer.parseInt(args[2])*1000);
 				return true;
 			}
 			else {
