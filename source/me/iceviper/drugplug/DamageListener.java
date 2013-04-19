a15fe6b6aa709e2913da9807f15c99f11b5a82af
1ceviper <jeroenvanwier@gmail.com>
2013-04-19 16:18:01 +0200

    Changed to common listener and added police functionaltiy

--- source/me/iceviper/drugplug/DamageListener.java
+++ source/me/iceviper/drugplug/DamageListener.java
@@ -1,5 +1,7 @@
 package me.iceviper.drugplug;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +10,18 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class DamageListener implements Listener {
+	
+	/*
+	 * Note: This was once created as a entityDamaged Listener
+	 * 		 but now listens to more events
+	 */
+	
+	private DrugPlug plugin;
+	
+	public DamageListener(DrugPlug plugin) {
+		this.plugin = plugin;
+	}	
+	
 	@EventHandler
 	public void entityDamaged(EntityDamageByEntityEvent e) {
 		Player police = null;
@@ -23,9 +37,34 @@ public class DamageListener implements Listener {
 				//Permission are good
 				if (police.getItemInHand().isSimilar(new ItemStack(Material.STICK,1))) {
 					//Police is holding a stick
-					
+					ItemStack scanning = null;
+					Boolean found = false;
+					for (int i = 0; i < 36; i++) {
+						scanning = smuggler.getInventory().getItem(i);
+						if(containsSimilar(plugin.drugs, scanning)) {
+							smuggler.getInventory().remove(scanning);
+							police.getWorld().dropItem(police.getLocation(), police.getInventory().addItem(scanning).get(0));
+							found = true;
+						}
+					}
+					if (found) {
+						jail(smuggler,1000*60*5);
+					} else {
+						//Police punishment
+					}
 				}
 			}
 		}
+	}
+	
+	public boolean containsSimilar(ArrayList<ItemStack> items, ItemStack scanning) {
+		for (ItemStack i : items) {
+			if (i.isSimilar(scanning)) return true;
+		}
+		return false;
+	}
+	
+	public void jail (Player player, int time) {
+		plugin.jailed.put(key, value);
 	}
 }
